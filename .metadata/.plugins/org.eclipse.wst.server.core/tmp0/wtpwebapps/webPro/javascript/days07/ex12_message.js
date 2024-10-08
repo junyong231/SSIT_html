678edb51cad2b2255f68eec037518e7d00101fd5
junyong231 <bagjunyong14@gmail.com>
2024-10-08 17:48:40 +0900

    js days07

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/webPro/javascript/days07/ex12_message.js
@@ -0,0 +1,14 @@
+/**
+ * ex12_message.js
+ * 함수 내보내기
+ */
+
+const message = () => {
+    const name = "홍길동";
+    const age = 20;
+    return `${name} / ${age}살`;
+
+}
+
+
+export default message;
---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/webPro/javascript/days07/ex12_person.js
@@ -0,0 +1,16 @@
+/**
+ * ex12_person.js
+ * 변수 내보내기 모듈 파일
+ */
+
+
+/*
+export const name = "admin";
+export const age = 20;
+*/
+
+
+const name = "admin";
+const age = 20;
+
+export{name, age};
